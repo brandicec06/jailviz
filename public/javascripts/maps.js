0f499b7b1dbb5671b8a78f446670b08c2c39bd31
brandicec06 <specv5@gmail.com>
2016-07-06 23:48:46 -0500

    color added to map

--- public/javascripts/maps.js
+++ public/javascripts/maps.js
@@ -1,6 +1,9 @@
   var height, path, projection, states, svg, width, pl;
 
-  states = d3.select('body').append('svg').append("svg").attr("id", "states");
+  states = d3.select('body').append('svg').append("svg").attr("id", "states")
+      .style("stroke","white")
+      .style("stroke-width", ".5")
+      .style("stroke-opacity", "0.4");;
 
   width = parseInt(window.innerWidth);
   height = parseInt(window.innerHeight);
@@ -14,7 +17,7 @@
 
   projection = d3.geoAlbers()
     .center([-10, 39])//-25
-    .scale(1500)
+    .scale(1400)
     .translate([width / 3, height / 2]);
 
 
@@ -22,12 +25,18 @@
   //console.log(projection.scale());
 
 
-  path = d3.geoPath().projection(projection);
-   // console.log(projection.invert([0,0]));
-   // console.log(projection.invert([width,height]));
+  path = d3.geoPath().projection(projection)
+
+  //console.log(projection.invert([0,0]));
+  // console.log(projection.invert([width,height]));
 
   var coord = [];
   var ncoord = [];
+  var extentsx = projection.invert([0,0]);
+  var extentsy = projection.invert([width,height]);
+  var r = 3;
+
+
 
   d3.json("./source/nstates.json", function(collection) { 
 
@@ -35,6 +44,7 @@
     for( var p in collection.features){
        var x = collection.features[p].properties.longitude;
        var y = collection.features[p].properties.latitude;
+
        coord.push({x,y});
      
     }  
@@ -73,6 +83,7 @@
     } 
   }
 
+
   d3.csv("./source/survey.csv", function(error,data){
       if(error) throw error;
 
@@ -85,42 +96,75 @@
       }
 
   coordScale(jails);
+  //console.log(extentsx);
+  //console.log(njails);
 
-  console.log(njails);
+  var rScale = d3.scaleLinear()
+    .domain([0,20000])
+    .range([3, 50])
 
-  var circles = svg.selectAll("circle").data(njails);
-        // Enter
-          circles.enter().append("circle")
-          .attr("r", 3)
-          .attr("cx", function (d){ 
-            return +d[0]; 
+
+  var over = function(){
+    var circle = d3.select(this);
+    pr = circle.attr("r");
+
+    circle.transition().duration(500)
+        .attr("r", circle.attr("r") * 1 + 10 );
+        //.style("opacity","0.2"); 
+  }
+
+
+  var out = function(){
+    var circle = d3.select(this);
+    circle.transition().duration(500)
+        .attr("r",pr)
+  }
+
+  var points;
+  var pr;
+  var circles = svg.selectAll("circle").data(data);
+        var c;
+
+        circles.enter().append("circle")
+          .attr("r", function(d,i){
+            return rScale(data[i].CONFPOP);
           })
-          .attr("cy", function (d){ return +d[1]; })
-          .style("fill", "blue")
-          .style('opacity', .5);
+          .attr("cx", function (d,i){ 
+
+            points = projection([data[i].LON,data[i].LAT]);
+            //console.log(points[0]);
+            
+            return points[0];
+          })
+          .attr("cy", function (d,i){ 
+            //console.log(points[0)
+            points = projection([data[i].LON,data[i].LAT]);
+            //console.log(points[1]);
+            return points[1];
+          })
+          .style("fill", function(d,i){
+            //console.log(d.CONFPOP)
+            
+            var t = d.CONFPOP;
+            var carr = ["#fed9a6","#b3cde3","#ccebc5","#decbe4","#fbb4ae"];
+
+            if(t<100){
+              c =0;
+            }else if( t< 500){
+              c =1;
+            }else if( t< 5000){
+              c =2;
+            }else if( t< 10000){
+              c =3;
+            }else{
+              c=4;
+            }
+            return carr[c];
+          })
+          .style('opacity', .5)
+          .on("mouseover",over)
+          .on("mouseout",out);
+
 
   });
 
-
-/*var width = 960,
-    height = 1160;
-
-var projection = d3.geoAlbers()
-    .center([0, 55.4])
-    .rotate([4.4, 0])
-    .parallels([50, 60])
-    .scale(1200 * 5)
-    .translate([width / 2, height / 2]);
-
-var path = d3.geoPath()
-    .projection(projection);
-
-var svg = d3.select("body").append("svg")
-    .attr("width", width)
-    .attr("height", height);
-
-d3.json("./source/nstates.json", function(error, us) {
-  svg.append("path")
-      .datum(topojson.feature(us, us.object.geometry))
-      .attr("d", path);
-});*/
