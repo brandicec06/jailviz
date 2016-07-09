99467e8863150d3a2dfa3b790fbf7c9b5dfffca8
brandicec06 <specv5@gmail.com>
2016-07-09 01:15:06 -0500

    added hover color isolation

--- public/javascripts/maps.js
+++ public/javascripts/maps.js
@@ -5,6 +5,10 @@
   var extentsy;
   var r = 3;
 
+  var carr = ["#fed9a6","#b3cde3","#fccde5","#ccebc5","ffffcc","e5d8bd","#decbe4","#fbb4ae"];
+  var dRange=[];
+  var interval= [];
+
   cLeft = 300;
   cTop = 75;
   cBot = 20;
@@ -12,9 +16,9 @@
   var name = "CONFPOP";
 
   states = d3.select('#map').append('svg').append("svg").attr("id", "states")
-      .style("stroke","white")
-      .style("stroke-width", ".5")
-      .style("stroke-opacity", "0.4");
+  .style("stroke","white")
+  .style("stroke-width", ".5")
+  .style("stroke-opacity", "0.4");
 
 
 
@@ -25,114 +29,130 @@
   njails = [];
 
   svg = d3.select('svg')
-    .attr("width", width)
-    .attr("height", height);
+  .attr("width", width)
+  .attr("height", height);
 
   projection = d3.geoAlbers()
     .center([-10, 39])//-25
     .scale(1400)
     .translate([width / 3, height / 2]);
 
-  extentsx = projection.invert([0,0]);
-  extentsy = projection.invert([width,height]);
+    extentsx = projection.invert([0,0]);
+    extentsy = projection.invert([width,height]);
 
-  //console.log(projection.scale());
+    path = d3.geoPath().projection(projection)
 
+    var gpts = [];
+    var gXp = 10;
+    var gYp = 10;
 
-  path = d3.geoPath().projection(projection)
-
-  // circle grid chart functionality
-
-
-  var gpts = [];
-  var gXp = 10;
-  var gYp = 10;
-
-  var gridScaleX = d3.scaleLinear()
+    var gridScaleX = d3.scaleLinear()
     .domain([0,10])
     .range([cLeft,width-cLeft/2])
 
-  var gridScaleY = d3.scaleLinear()
+    var gridScaleY = d3.scaleLinear()
     .domain([0,10])
     .range([cTop,height-cBot]);
 
 
-  function gridPoints(){
+    function gridPoints(){
       for(var i =0; i<10; i++){
         var x = gridScaleX(i);
         for(var j =0; j<10; j++){
           var y = gridScaleY(j);
-            gpts.push({x,y});
+          gpts.push({x,y});
         }
       }
 
       return gpts;
-  }
+    }
 
-  d3.json("./source/nstates.json", function(collection) { 
+    d3.json("./source/nstates.json", function(collection) { 
 
 
-    for( var p in collection.features){
+      for( var p in collection.features){
        var x = collection.features[p].properties.longitude;
        var y = collection.features[p].properties.latitude;
 
        coord.push({x,y});
-     
-    }  
 
-    states.selectAll("path").data(collection.features).enter().append("svg:path")
-      .attr("d", path)
-      .append("svg:title")
-      .text(function(d) {
-        return d.properties.name;
+     }  
+
+     states.selectAll("path").data(collection.features).enter().append("svg:path")
+     .attr("d", path)
+     .append("svg:title")
+     .text(function(d) {
+      return d.properties.name;
     });
 
-  });
+   });
 
 
 
-  function coordScale(pts){
-    for(i in pts){
-      njails.push(projection([pts[i].jx,pts[i].jy]));
-    } 
-  }
-
-  function rMap(num,bounds, key){
-
-    if(key == "NCONPOP" || key == "UNCONV" || key == "BLACK"){
-      var nBound = [0,20000];
-      bounds = nBound;
+    function coordScale(pts){
+      for(i in pts){
+        njails.push(projection([pts[i].jx,pts[i].jy]));
+      } 
     }
 
-    var dScale = d3.scaleLinear()
-    .domain(bounds)
-    .range([3, 50])
+    function rMap(num,bounds, key){
 
-    var sNum = dScale(num);
+      if(key == "NCONPOP" || key == "UNCONV" || key == "BLACK"){
+        var nBound = [0,20000];
+        bounds = nBound;
+      }
 
-    return sNum;
-  }
+      var dScale = d3.scaleLinear()
+      .domain(bounds)
+      .range([3, 50])
+
+      var sNum = dScale(num);
+
+      return sNum;
+    }
 
 
-  function extents(obj, k){
-    var range = [];
-    var arr = _.pluck(obj,k);
-    arr = arr.map(Number);
+    function extents(obj, k){
+      var range = [];
+      var arr = _.pluck(obj,k);
+      arr = arr.map(Number);
 
 
-    range.push(_.min(arr));
-    range.push(_.max(arr));
-    return range;
+      range.push(_.min(arr));
+      range.push(_.max(arr));
+      return range;
     //return range[rMax,rMin];
   }
   //adds div for tool tip 
   var div = d3.select("body").append("div") 
-    .attr("class", "tooltip")       
-    .style("opacity", 0);
+  .attr("class", "tooltip")       
+  .style("opacity", 0);
+
+  function colorMap(color){
+
+    if(color<interval[1]){
+      c =0;
+    }else if( color< interval[2]){
+      c =1;
+    }else if( color< interval[3]){
+      c =2;
+    }else if( color< interval[4]){
+      c =3;
+    }else if( color< interval[5]){
+      c =4;
+    }else if( color< interval[6]){
+      c =5;
+    }else{
+      c=6;
+    }
+    console.log(c+"__"+color+"__"+interval);
+    return carr[c];
+  }
 
 
-function update(key,chart){
-  d3.csv("./source/survey.csv", function(error,data){
+
+  function update(key,chart){
+    d3.csv("./source/survey.csv", function(error,data){
       if(error) throw error;
 
       for( p in data){
@@ -143,9 +163,10 @@ function update(key,chart){
         
       }
 
-  coordScale(jails);
+      coordScale(jails);
 
-  var dRange = extents(data,key);
+      dRange = extents(data,key);
+      interval = _.range(dRange[0],dRange[1], dRange[1]/carr.length);
   //console.log(dRange);
 
   var over = function(){
@@ -153,52 +174,51 @@ function update(key,chart){
     pr = circle.attr("r");
 
     circle.transition().duration(500)
-        .attr("r", circle.attr("r") * 1 + 10 );
+    .attr("r", circle.attr("r") * 1 + 10 );
         //.style("opacity","0.2"); 
-  }
+      }
 
-  var out = function(){
-    var circle = d3.select(this);
-    circle.transition().duration(500)
+      var out = function(){
+        var circle = d3.select(this);
+        circle.transition().duration(500)
         .attr("r",pr)
-  }
+      }
 
-  var points;
-  var pr;
-  var circles = svg.selectAll("circle").data(data);
-        var c;
+      var points;
+      var pr;
+      var circles = svg.selectAll("circle").data(data);
+      var c;
 
-          var ppts =[];
-          var Gpoint = [];
-          if(chart == true){
-              states.selectAll("path").transition().duration(1000)
-                .style("stroke-opacity", "0");
-              gridPoints();
-          }else{
-              states.selectAll("path").transition().duration(1000)
-                .style("stroke-opacity", "0.4");
-          }
-   
-        circles.enter().append("circle")
-          .attr("r", 0)
-          .attr("cx", function (d,i){ 
+      var ppts =[];
+      var Gpoint = [];
+      if(chart == true){
+        states.selectAll("path").transition().duration(1000)
+        .style("stroke-opacity", "0");
+        gridPoints();
+      }else{
+        states.selectAll("path").transition().duration(1000)
+        .style("stroke-opacity", "0.4");
+      }
 
-            points = projection([data[i].LON,data[i].LAT]);
+      circles.enter().append("circle")
+      .attr("r", 0)
+      .attr("cx", function (d,i){ 
+
+        points = projection([data[i].LON,data[i].LAT]);
             //console.log(points[0]);
             
             return points[0];
           })
-          .attr("cy", function (d,i){ 
+      .attr("cy", function (d,i){ 
             //console.log(points[0)
             points = projection([data[i].LON,data[i].LAT]);
             //console.log(points[1]);
             return points[1];
           })
-          .style("fill", function(d,i){
+      .style("fill", function(d,i){
             //console.log(d.name)
             
             var t = d[name];
-            var carr = ["#fed9a6","#b3cde3","#ccebc5","#decbe4","#fbb4ae"];
 
             if(t<100){
               c =0;
@@ -213,55 +233,70 @@ function update(key,chart){
             }
             return carr[c];
           })
-          .style("opacity",.5)
-          .on("mouseover",over)
-          .on("mouseout",out)
+      .style("opacity",.5)
+         // .on("mouseover",over)
+         .on("mouseout",out)
 
-          svg.selectAll("circle").on("mouseover",function(d) {   
-            div.transition()    
-                .duration(200)    
-                .style("opacity", .9)
-            div .html(d[key]+ "<br/>"  + d["CITY"])  
-                .style("left", (d3.event.pageX) + "px")   
-                .style("top", (d3.event.pageY - 28) + "px")
+         svg.selectAll("circle").on("mouseover",function(d,i) {   
+          div.transition()    
+          .duration(200)    
+          .style("opacity", .9)
+          div .html(d[key]+ "<br/>"  + d["CITY"])  
+          .style("left", (d3.event.pageX + 30) +"px")   
+          .style("top", (d3.event.pageY - 28) + "px")
+          d3.selectAll("circle")
+          .style('opacity',function(r,i){
+               // console.log(match + '__' + colorMap(r[key],true))
+               if(d.color != r.color){
+                return .1;
+              }else{
+                return .6;
+              }
             })
-          .on("mouseout", function(d) {   
-            div.transition()    
-                .duration(500)    
-                .style("opacity", 0); 
-           })
-          .transition()
-          .duration(2000);
 
-        svg.selectAll("circle").transition().duration(1000)
-          .attr("r", function(d,i){
+        })
+         .on("mouseout", function(d) {   
+          div.transition()    
+          .duration(500)    
+          .style("opacity", 0); 
+          d3.selectAll("circle"). transition().duration(800)
+          .style('opacity',function(d){
+            return .5;
+          })
+        })
+         .transition()
+         .duration(2000);
+
+         svg.selectAll("circle").transition().duration(1000)
+         .attr("r", function(d,i){
 
               //console.log(rMap(data[i][key], dRange));
-            return rMap(data[i][key], dRange,key);
-          })
-          .attr("cx", function (d,i){ 
-            var xc;
-            if(chart){
-              if(i <100){
-                points = [gpts[i]["x"],gpts[i]["y"]];
-                xc = points[0]
-              }else{
-                xc = -100;
-              }
+              return rMap(data[i][key], dRange,key);
+            })
+         .attr("cx", function (d,i){ 
+          var xc;
+          if(chart){
+            if(i <100){
+              points = [gpts[i]["x"],gpts[i]["y"]];
+              xc = points[0]
             }else{
-              points = projection([data[i].LON,data[i].LAT]);
-              xc = points[0];
+              xc = -100;
+            }
+          }else{
+            points = projection([data[i].LON,data[i].LAT]);
+            xc = points[0];
           }
             //console.log(points[0]);
-    
+
             return xc;
           })
-          .attr("cy", function (d,i){ 
-            if(chart){
-              var yc;
-              if(i <100){
-                points = [gpts[i]["x"],gpts[i]["y"]];
-                yc = points[1]
+         .attr("cy", function (d,i){ 
+
+          if(chart){
+            var yc;
+            if(i <100){
+              points = [gpts[i]["x"],gpts[i]["y"]];
+              yc = points[1]
                 //console.log(points);
               }else{
                 yc = -100;
@@ -273,22 +308,9 @@ function update(key,chart){
             return yc;
           }).style("fill", function(d,i){
             //console.log(d.name)
-            
             var t = d[key];
-            var carr = ["#fed9a6","#b3cde3","#ccebc5","#decbe4","#fbb4ae"];
-
-            if(t<100){
-              c =0;
-            }else if( t< 500){
-              c =1;
-            }else if( t< 5000){
-              c =2;
-            }else if( t< 10000){
-              c =3;
-            }else{
-              c=4;
-            }
-            return carr[c];
+            d["color"] = colorMap(t);
+            return colorMap(t);
           })
           .style("opacity",function(d,i){
             if(chart ){
@@ -296,22 +318,15 @@ function update(key,chart){
                return .5;
              }else{
               return 0;
-             }
-            }else {
-              return .5;
             }
-          });
-
-
-
-        circles.exit().remove();
-
-
-
-    });
-    console.log(key);
-    return key;
- } 
+          }else {
+            return .5;
+          }
+        });
+          circles.exit().remove();
+        });
+  return key;
+} 
 
 
 function updateData(name,bool){
